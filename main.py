fb908b7ef5ad83b4a834ec7ab8557e3ac5173da3
ytraub <y.traub@outlook.com>
2026-03-31 18:32:03 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,111 @@
+import subprocess
+
+from textual.message import Message
+from textual.renderables.gradient import LinearGradient
+from textual.app import App, ComposeResult, RenderResult
+from textual.containers import Container
+from textual.events import Event, Key
+from textual.widget import Widget
+from textual.widgets import Footer, Label, ListItem, ListView
+
+
+COLORS = [
+    "#881177",
+    "#aa3355",
+    "#cc6666",
+    "#ee9944",
+    "#eedd00",
+    "#99dd55",
+    "#44dd88",
+    "#22ccbb",
+    "#00bbcc",
+    "#0099cc",
+    "#3366bb",
+    "#663399",
+]
+STOPS = [(i / (len(COLORS) - 1), color) for i, color in enumerate(COLORS)]
+
+
+class Title(Widget):
+    def render(self) -> RenderResult:
+        return """
+                            _
+    /\\                     | |
+   /  \\   _ __ ___ __ _  __| | ___
+  / /\\ \\ | '__/ __/ _` |/ _` |/ _ \\
+ / ____ \\| | | (_| (_| | (_| |  __/
+/_/    \\_\\_|  \\___\\__,_|\\__,_|\\___|
+"""
+
+
+class Splash(Container):
+    def on_mount(self) -> None:
+        pass
+
+    def compose(self) -> ComposeResult:
+        yield Title()
+        yield GameList()
+
+    def render(self) -> RenderResult:
+        return LinearGradient(62, STOPS)
+
+
+class GameListItem(ListItem):
+    def __init__(self, label: str) -> None:
+        super().__init__()
+        self.label_text = label
+        self.label_widget = Label(label)
+
+    def compose(self) -> ComposeResult:
+        yield self.label_widget
+
+    def set_highlighted(self, state: bool) -> None:
+        if state:
+            self.label_widget.update("➤  " + self.label_text)
+        else:
+            self.label_widget.update(self.label_text)
+
+
+class GameList(Widget):
+    def compose(self) -> ComposeResult:
+        self.list_view = ListView(
+            GameListItem("Hello"),
+            GameListItem("Hello"),
+            GameListItem("Hello"),
+            GameListItem("Hello"),
+            GameListItem("Hello"),
+        )
+        yield self.list_view
+
+    def on_list_view_highlighted(self, event: ListView.Highlighted) -> None:
+        for item in self.list_view.children:
+            item.set_highlighted(False) # type: ignore
+            item.remove_class("highlighted-item")
+
+        if event.item:
+            event.item.set_highlighted(True) # type: ignore
+            event.item.add_class("highlighted-item")
+            
+    def on_list_view_selected(self, event: ListView.Highlighted) -> None:
+        if event.item:
+            subprocess.run(["pico8"])
+
+
+class Main(App):
+    CSS_PATH = "styles.tcss"
+
+    def on_mount(self) -> None:
+        self.theme = "atom-one-dark"
+
+    def compose(self) -> ComposeResult:
+        yield Splash()
+
+    def on_key(self, event: Key) -> None:
+        match event.key:
+            case "q":
+                self.exit()
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.run()
