dc76f8e205f74bf361a9a8150f1b8c3c30b6b4f4
ytraub <y.traub@outlook.com>
2026-06-29 12:13:50 +0200

    Better names

--- Names.py
+++ Names.py
@@ -1,16 +1,28 @@
 import random
-from Database import *
+
+import Database as db
 
 ADJECTIVES = "names/adjectives.txt"
 NOUNS = "names/nouns.txt"
 
-def load_words():
-    with open("words.txt", "r", encoding="utf-8") as f:
-        return [line.strip() for line in f if line.strip()]
 
-WORD_LIST = load_words()
+def load_words() -> tuple[list[str], list[str]]:
+    with open(ADJECTIVES, encoding="utf-8") as f:
+        adjectives = [line.strip() for line in f if line.strip()]
 
-def generate():
-    name = random.choice(WORD_LIST) + random.choice(WORD_LIST)
-    if not is_name_taken(name) :
-        return name
+    with open(NOUNS, encoding="utf-8") as f:
+        nouns = [line.strip() for line in f if line.strip()]
+
+    return adjectives, nouns
+
+
+adjectives, nouns = load_words()
+
+
+def generate() -> str:
+    while True:
+        name = random.choice(adjectives) + random.choice(nouns)
+
+        if not db.is_name_taken(name):
+            return name
+
