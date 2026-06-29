e6c72c6b062a0e7280bf5519ce11fdf328ddca27
BannanenBrot <87713248+Nulldi@users.noreply.github.com>
2026-06-29 14:51:47 +0200

    game fix final

--- games/slot_machine/src/main.lua
+++ games/slot_machine/src/main.lua
@@ -1,15 +1,35 @@
+name = ""
+coins = 0
+
 function _init()
+   serial(0x806, 0x9a00, 64)
+
     local text = ""
 
-    for i=0, 7 do
-        text = text .. chr(peek(0x9a00 + i))
+    for i=0,63 do
+        local c = peek(0x9a00 + i)
+
+        if c == 0 then
+            break
+        end
+
+        text = text .. chr(c)
     end
 
-    local pos = text:find(":")
+    printh("received=[" .. text .. "]")
+
+    local pos
+
+    for i=1,#text do
+        if sub(text, i, i) == ":" then
+            pos = i
+            break
+        end
+    end
 
     if pos then
         name = sub(text, 1, pos - 1)
-        coins = sub(text, pos + 1)
+        coins = sub(text, pos + 1) + 0
     end
 end
 
